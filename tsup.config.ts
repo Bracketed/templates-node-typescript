e248a3e0365e291415a8c6d330bf1b2a9213c2e1
ninjaninja140 <88348015+ninjaninja140@users.noreply.github.com>
2025-06-22 22:54:05 +0100

    Refactors build process with tsup configuration
    Streamlines the build process by adopting a shared tsup configuration.
    
    This change simplifies the build setup, improves maintainability,
    and ensures consistency across projects by leveraging a centralized
    tsup configuration. Removes redundant build plugins and configurations.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,26 +1,3 @@
-import { esbuildPluginFilePathExtensions } from 'esbuild-plugin-file-path-extensions';
-import { esbuildPluginVersionInjector } from 'esbuild-plugin-version-injector';
-import { defineConfig, type Options } from 'tsup';
+import { moduleJsConfiguration } from '@bracketed/tsup-configuration';
 
-const baseOptions: Options = {
-	clean: true,
-	entry: ['src/**/*.ts'],
-	dts: true,
-	minify: true,
-	skipNodeModulesBundle: true,
-	sourcemap: true,
-	target: 'esnext',
-	tsconfig: 'tsconfig.json',
-	keepNames: true,
-	esbuildPlugins: [esbuildPluginVersionInjector(), esbuildPluginFilePathExtensions()],
-	treeshake: true,
-	bundle: true,
-};
-
-export default [
-	defineConfig({
-		...baseOptions,
-		outDir: 'dist',
-		format: 'esm',
-	}),
-];
+export default [...moduleJsConfiguration];
